32380d94fa143bd6c1a6308da0bebdc5b827e055
Tamara <rntamara93@gmail.com>
2025-08-09 16:56:07 -0300

    Corrigindo o bug

--- service/transferService.js
+++ service/transferService.js
@@ -4,7 +4,7 @@ const transfers = require('../model/transferModel');
 function transfer({ from, to, amount }) {
   const sender = users.find(u => u.username === from);
   const recipient = users.find(u => u.username === to);
-  if (!sender || !recipient) throw new Error('Usuário não está encontrado');
+  if (!sender || !recipient) throw new Error('Usuário não encontrado');
   if (sender.balance < amount) throw new Error('Saldo insuficiente');
   if (!recipient.isFavored && amount >= 5000) throw new Error('Transferência acima de R$ 5.000,00 só para favorecidos');
   sender.balance -= amount;
